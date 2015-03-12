a5698f6faa2d3ad590030b0e06ad5b431d7c09d0
Kovan Azeez <kovan.aziz08@gmail.com>
2015-03-11 17:40:21 -0700

    create_arrayadapter

--- app/src/main/java/com/example/hma/sunshine/MainActivity.java
+++ app/src/main/java/com/example/hma/sunshine/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -66,6 +67,8 @@ public class MainActivity extends ActionBarActivity {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             String[] forecastArray={"Today - Sunny - 88/63","Tomorrow - Foggy - 70/46","Weds - Cloudy - 72/63","Thurs - Rainy - 64/51","Friday - Foggy - 70/63","Saturday - Sunny - 88/63"};
             List<String> weekForcast=new ArrayList<String>(Arrays.asList(forecastArray));
+            ArrayAdapter<String> MyAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForcast);
+
             return rootView;
         }
     }
